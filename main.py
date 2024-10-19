3c3479b9ebc482efdabc7ed16bde77f16d7f1d85
pradpradprad <dissayaprad.ds@gmail.com>
2024-10-19 20:25:36 +0700

    add list

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from my_module.operator import add, subtract
 result1 = add(5, 3)
 result2 = subtract(10, 4)
 
-print(f"Addition Result: {result1}")
-print(f"Subtraction Result: {result2}")
+my_list = [1,2,3,4,5]
+print(len(my_list))
