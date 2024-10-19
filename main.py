c528a4b1506de872c3c3a99f74acc42208632e55
pradpradprad <dissayaprad.ds@gmail.com>
2024-10-19 20:26:51 +0700

    add hello statement

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from my_module.operator import add, subtract
 result1 = add(5, 3)
 result2 = subtract(10, 4)
 
+print('Hello, hi')
+
 print(f"Addition Result: {result1}")
 print(f"Subtraction Result: {result2}")
